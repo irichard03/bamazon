975ddcf043f2bd9f03911eb0aa90dfc064fc65d0
Ian Richard <ianrichard03@gmail.com>
2018-11-03 18:45:29 -0500

    added managerMode.js to create manager functions.

---
+++ bamazonManager.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const managerMode = require('./managerMode');
+
+const myManager = new managerMode();
+
+myManager.display();
+
+
+
+//close create connection
+
+
+//list menu options
+
+//view products for sale
+    //list all items, their ID, prices and quantity.
+//view low inventory
+    //list all items with inventory lower than 5.
+//add to inventory
+    //display prompt to add inventory to any item
+//add new product
+    //add completely new product to store.
---
+++ managerMode.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const managerMode = function(){
+     this.connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    });
+
+    this.display = function(){
+        console.log("I'm an object!");
+    }
+}
+
+module.exports = managerMode;
